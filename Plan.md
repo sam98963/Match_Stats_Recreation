cebd49cb4fb9787664e61f588809fae5a09fc8b8
sam98963 <szwilliamson01@gmail.com>
2023-08-31 13:02:42 +0100

    installed jest and pulled data

--- Plan.md
+++ Plan.md
@@ -1,7 +1,7 @@
 ### Session 1 Plan
-- Initialise vite project, clear app.js and app.css files etc.
-- Write test for api call.
+- Initialise vite project, clear app.js and app.css files etc. ✅
 - Pull all data from API and diplay in console.
+- Write test for api call.
 - Write tests propdata rendering in header component.
 - Begin with the first "header" component.
 - Render relevent data on page.
--- match-stats-recreation/src/App.jsx
+++ match-stats-recreation/src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css'
-
+import {useState, useEffect} from "react"
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  async function fetchData(){
+    const response = await fetch("https://frontend-tech-test-ashy.vercel.app/api/match")
+    const jsonData = await response.json()
+    setData(jsonData)
+  }
+
+  console.log(data)
 
   return (
     <>
