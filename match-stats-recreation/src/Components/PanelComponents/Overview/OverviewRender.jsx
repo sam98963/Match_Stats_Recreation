2c49210e2ca98f7022fb83e2ac186775421736a3
sam98963 <szwilliamson01@gmail.com>
2023-09-01 15:20:30 +0100

    cards and goals shown

--- match-stats-recreation/src/Components/PanelComponents/Overview/OverviewRender.jsx
+++ match-stats-recreation/src/Components/PanelComponents/Overview/OverviewRender.jsx
@@ -2,13 +2,36 @@ import PropTypes from "prop-types"
 
 export default function OverviewRender({data}){
 
+  const yellowCards = data.cards.filter((item) => item.type === "YC");
+  const redCards = data.cards.filter((item) => item.type === "Y2C" || item.type === "RC");
+
+    console.log("This is goals", data.cards)
   return(
   <>
-    {/* <div>
-      {data.goals.map(item=>{
-        <p>{}</p>
-      })}
-    </div> */}
+    <div>
+      {data? data.goals.map((item, index)=>(
+        item.scorerName? 
+        <div key = {index}>
+        <p>({item.timeMin}&rsquo;) {item.scorerName}</p>
+        </div> : <p key = {index}>Anon</p>
+      )): null}
+    </div>
+    <div>
+      {yellowCards? yellowCards.map((item, index)=>(
+        item.playerName? 
+        <div key = {index}>
+        <p>({item.timeMin}&rsquo;) {item.playerName} {item.type}</p>
+        </div> : <p key = {index}>({item.timeMin}&rsquo;) Anon {item.type}</p>
+      )): null}
+    </div>
+    <div>
+      {redCards? redCards.map((item, index)=>(
+        item.playerName? 
+        <div key = {index}>
+        <p>({item.timeMin}&rsquo;) {item.playerName} {item.type}</p>
+        </div> : <p key = {index}>({item.timeMin}&rsquo;) Anon {item.type}</p>
+      )): null}
+    </div>
   </>
   )
 }
